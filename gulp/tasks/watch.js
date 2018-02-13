e55e15bf74ed0d8c6a1364e7c56d8f66c16061c2
Gary Coleman <gryclmn@hotmail.com>
2018-02-12 16:33:57 -0800

    Styling header. Learning webpack to build Javascript modules

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,7 +17,11 @@ gulp.task('watch', function() {
     
     watch('./app/assets/styles/**/*.css', function() {
         gulp.start('cssInject');
-    })
+    });
+    
+    watch('./app/assets/scripts/**/*.js', function() {
+        browserSync.reload();
+    });
 });
 
 gulp.task('cssInject', ['styles'], function() {
