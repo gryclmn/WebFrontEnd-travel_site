fb20ec72d66ac8e37b6ec8122c60c0d2daf60d93
Gary Coleman <gryclmn@hotmail.com>
2018-02-16 13:32:43 -0800

    Basic reveal-on-scroll functionality added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var movileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
